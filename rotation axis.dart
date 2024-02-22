12bef12e5260cdb7b84ea22e243173f0dac88087
Marco Rossi <160949726+Marco25rrrrossi@users.noreply.github.com>
2024-02-22 21:52:50 +0200

    Update rotation axis.dart

--- rotation axis.dart
+++ rotation axis.dart
@@ -9,7 +9,24 @@ import 'config.dart';
 class ItemDetailPane extends StatelessWidget {
   final Item item;
 
+
+  //x axis
   const ItemDetailPane({required this.item, Key? key}) : super(key: key);
 
   final Map<String, int> longFilters;
   final Map<String, String> stringFilters;
+
+  //z axis
+
+    @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+  //y axis
+    Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
